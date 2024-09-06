2ffb534b82166613216d86e9d587b23b666b461e
arthur972645 <arthurall.1973@gmail.com>
2024-09-06 10:14:50 -0300

    RF03 finalizado

--- Backend/src/routes/PostagensRoutes.js
+++ Backend/src/routes/PostagensRoutes.js
@@ -3,7 +3,10 @@ import { Router } from "express";
 import {
     cratePostagem,
     showall,
-    getbyid
+    getbyid,
+    atualizarinformacoes,
+    deleteinformacoes,
+    imagesend
 } from "../controllers/PostagensControllers.js"
 
 const router = Router()
@@ -11,5 +14,8 @@ const router = Router()
 router.post("/", cratePostagem)
 router.get("/", showall)
 router.get("/:id", getbyid)
+router.delete("/:id", deleteinformacoes)
+router.put("/:id", atualizarinformacoes)
+router.post("/:id/imagem",  bodyParser.raw({type: ["image/jpeg", "image/png"], limit: "5mb"}),imagesend)
 
 export default router;
